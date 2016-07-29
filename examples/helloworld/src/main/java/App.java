fd278555c87297cb21867d697ed7e2038870add9
Warren MacEvoy <wmacevoy@gmail.com>
2016-07-28 21:14:12 -0600

    test hello

--- examples/helloworld/src/main/java/App.java
+++ examples/helloworld/src/main/java/App.java
@@ -1,6 +1,16 @@
 import static kiss.API.*;
 
 class App {
+    void testRun() {
+        outOpen("testRun.verify");
+        println("Hello, World!");
+        outClose();
+
+        outVerify("testRun.verify");
+        run();
+        outClose();
+    }
+    
     void run() {
         println("Hello, World!");
     }
