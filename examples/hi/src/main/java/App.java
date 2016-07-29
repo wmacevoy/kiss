2419bb56bdda59a9e6b91251ab630dce7e914746
Warren MacEvoy <wmacevoy@gmail.com>
2016-07-28 21:44:19 -0600

    update hi for testing

--- examples/hi/src/main/java/App.java
+++ examples/hi/src/main/java/App.java
@@ -1,7 +1,31 @@
 import static kiss.API.*;
 
 class App {
-    void run() {
-        println("Hello, World!");
+    void testHiAda() {
+        // create a file with test input
+        outOpen("hi.in");
+        println("Ada");
+        outClose();
+
+        // create a file with expected output
+        outOpen("hi.out");
+        println("What is your name? Ada");
+        println("Hello, Ada!");
+        outClose();
+
+        inOpen("hi.in"); // future input
+        outVerify("hi.out"); // expected output
+        hi(); 
+        outClose();
+        inClose();
     }
+
+    void hi()
+    {
+        print("What is your name? ");
+        String name = readLine();
+        println("Hello, " + name + "!");
+    }
+
+    // no run() -- we just want to test Hi
 }
