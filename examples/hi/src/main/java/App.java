a7b2f179c375ac0a7e009eda311d538f33fb20cc
Warren MacEvoy <wmacevoy@gmail.com>
2016-08-02 17:04:09 -0600

    better supply/expect example

--- examples/hi/src/main/java/App.java
+++ examples/hi/src/main/java/App.java
@@ -2,8 +2,9 @@ import static kiss.API.*;
 
 class App {
     void testHiAda() {
-        inProvide("Ada");
-        outExpect("What is your name? Hello, Ada!");
+        inProvide("Ada",EOL,42);
+        outExpect("What is your name? What is a number? Hello, Ada!",EOL,
+                  "I like 42 too.");
         hi();
         outClose();
         inClose();
@@ -13,7 +14,10 @@ class App {
     {
         print("What is your name? ");
         String name = readLine();
+        print("What is a number? ");
+        int number = readInt(); readEOL();
         println("Hello, " + name + "!");
+        println("I like " + number + " too.");
     }
 
     // no run() -- we just want to test Hi
