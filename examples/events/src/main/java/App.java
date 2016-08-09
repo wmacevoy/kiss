cf4b0f0028bdb8485f5068491616d0ec59e93322
Warren MacEvoy <wmacevoy@gmail.com>
2016-08-09 07:38:56 -0600

    simpler event example

--- examples/events/src/main/java/App.java
+++ examples/events/src/main/java/App.java
@@ -1,142 +1,40 @@
 import static kiss.API.*;
 
-// kind of information to pass around
-enum EnvironmentState { Cold, Comfortable, Hot };
+enum State { off, on };
 
-// something which gets this kind of information
-class Fan {
-    String name;
-    boolean isOn = false;
+class Light {
+    String location;
 
-    Fan(String _name) { name=_name; }
+    Light(String _location) { location=_location; }
 
-    void onReceiveEnvironmentState(Thermostat thermostat,
-                                   EnvironmentState state) {
-        if (isOn) {
-            if (state == EnvironmentState.Cold) {
-                println(name + " fan turned off");
-                isOn = false;
-            }
-        } else {
-            if (state == EnvironmentState.Hot) {
-                println(name + " fan turned on");
-                isOn = true;
-            }
-        }
+    void onReceiveState(Remote remote, State state) {
+        println("light in " + location + " is " + state);
     }
 }
 
-class Heater {
-    String name;
-    boolean isOn = false;
-    Heater(String _name) { name = _name; }
-    
-    void onReceiveEnvironmentState(Thermostat thermostat,
-                                   EnvironmentState state) {
-        if (isOn) {
-            if (state == EnvironmentState.Hot) {
-                println(name + " heater turned off");
-                isOn = false;
-            }
-        } else {
-            if (state == EnvironmentState.Cold) {
-                println(name + " heater turned on");
-                isOn = true;
-            }
-        }
-    }
+class Remote extends Generator<State> {
+    void on() { send(State.on); }
+    void off() { send(State.off); }
 }
-
-
-// Something that sends/generates environment state info,
-// default generator helps Thermostat keep track of interested
-// listeners
-
-class Thermostat extends Generator<EnvironmentState> {
-    private double temperature; // degrees centigrade
-    private double tooHot;
-    private double tooCold;
-    private EnvironmentState state;
-
-    Thermostat() { // Constructor
-        temperature = 25;
-        tooHot = 30;
-        tooCold = 20;
-        state = EnvironmentState.Comfortable;
-    }
-
-    void setTemperature(double _temperature) {
-        temperature = _temperature;
-        adjustState();
-    }
-
-    void setTooHot(double _tooHot) {
-        tooHot = _tooHot;
-        adjustState();
-    }
-
-    void setTooCold(double _tooCold) {
-        tooCold = _tooCold;
-        adjustState();
-    }
-
-    private void adjustState() {
-        EnvironmentState _state;
-        if (temperature >= tooHot) {
-            _state = EnvironmentState.Hot;
-        } else if (temperature <= tooCold) {
-            _state = EnvironmentState.Cold;
-        } else {
-            _state = EnvironmentState.Comfortable;
-        }
-        if (state != _state) {
-            state=_state;
-            // we found out the environment state changed,
-            // so if anyone is listening, send the change
-            if (listening()) send(state);
-        }
-    }
-
-    boolean isHot() { return state == EnvironmentState.Hot; }
-    boolean isCold() { return state == EnvironmentState.Cold; }
-}
-
 
 class App {
     void testHouse() {
-        Fan bedroomFan = new Fan("bedroom");
-        Fan garageFan = new Fan("garage");
-        Heater bedroomHeater = new Heater("bedroom");
-        Heater garageHeater = new Heater("garage");
-        Thermostat bedroomThermostat = new Thermostat();
-        Thermostat garageThermostat = new Thermostat();
+        Light bedroomLight = new Light("bedroom");
+        Light kitchenLight = new Light("kitchen");
 
-        garageThermostat.setTooCold(5);
-        garageThermostat.setTooHot(50);
-        garageThermostat.addListener(garageFan);
-        garageThermostat.addListener(garageHeater);
+        Remote lightsRemote = new Remote();
 
-        bedroomThermostat.setTooCold(22);
-        bedroomThermostat.setTooHot(28);
-        bedroomThermostat.addListener(bedroomFan);
-        bedroomThermostat.addListener(bedroomHeater);
+        lightsRemote.addListener(bedroomLight);
+        lightsRemote.addListener(kitchenLight);
 
-        try (Close out = outExpect("garage heater turned on")) {
-            garageThermostat.setTemperature(-10);
+        try (Close out = outExpect("light in bedroom is on",EOL,
+                                   "light in kitchen is on",EOL)) {
+            lightsRemote.on();
         }
 
-        try (Close out = outExpect("garage fan turned on",EOL,
-                                   "garage heater turned off")) {
-            garageThermostat.setTemperature(100);
-        }
-
-        try (Close out = outExpect("bedroom heater turned on")) {
-            bedroomThermostat.setTemperature(20);
-        }
-
-        try (Close out = outExpect("bedroom fan turned on",EOL,
-                                   "bedroom heater turned off")) {
-            bedroomThermostat.setTemperature(30);
+        try (Close out = outExpect("light in bedroom is off",EOL,
+                                   "light in kitchen is off",EOL)) {
+            lightsRemote.off();
         }
     }
 }
