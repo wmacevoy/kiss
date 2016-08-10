d6d7468816fd576e54f0308e5d03749e651addc9
Warren MacEvoy <wmacevoy@gmail.com>
2016-08-09 22:13:28 -0600

    better events app

--- examples/events/src/main/java/App.java
+++ examples/events/src/main/java/App.java
@@ -1,7 +1,9 @@
 import static kiss.API.*;
 
+// A State can be on or off...
 enum State { off, on };
 
+// A Light can receive State messages...
 class Light {
     String location;
 
@@ -12,6 +14,14 @@ class Light {
     }
 }
 
+// A Remote extends Generator<State> to generate State messages, so
+//
+//   * Any Light (or other objects with an onReceiveState(...)) can
+//     listen to a Remote to receive State messages.
+//
+//   * Remote can send(State msg) to send a message to all added
+//     listeners.
+//
 class Remote extends Generator<State> {
     void on() { send(State.on); }
     void off() { send(State.off); }
@@ -24,16 +34,22 @@ class App {
 
         Remote remote = new Remote();
 
+        // connecting remote to lights...
         remote.addListener(bedroomLight);
         remote.addListener(kitchenLight);
 
+        // and an arbitrary action (lambda function)
+        remote.addListener((state)->println("sent " + state));
+
         try (Close out = outExpect("light in bedroom is on",EOL,
-                                   "light in kitchen is on",EOL)) {
+                                   "light in kitchen is on",EOL,
+                                   "sent on")) {
             remote.on();
         }
 
         try (Close out = outExpect("light in bedroom is off",EOL,
-                                   "light in kitchen is off",EOL)) {
+                                   "light in kitchen is off",EOL,
+                                   "sent off")) {
             remote.off();
         }
     }
